52e5493b530a21036187b8e749106cae270d7166
Rafael Cruz <ralph1786@gmail.com>
2018-11-24 22:13:07 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,6 @@ end
 def monopoly
 	monopoly = {railroads: {}
 	}
-
 end
 
 def monopoly_with_second_tier
@@ -32,7 +31,6 @@ def monopoly_with_third_tier
 			rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
 		}
 	}
-		
 end
 
 def monopoly_with_fourth_tier
@@ -48,7 +46,6 @@ def monopoly_with_fourth_tier
 			rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
 		}
 	}
-	
 end
 
 
